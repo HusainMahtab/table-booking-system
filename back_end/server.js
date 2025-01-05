29b455b8c4eb0818b167f8e0c16ace18712c4998
ahkhan13 <khanahs3351@gmail.com>
2025-01-05 19:46:18 +0530

    deployment setup

--- back_end/server.js
+++ back_end/server.js
@@ -8,7 +8,7 @@ dotenv.config({ path: "./.env" });
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:5173', ''],
+    origin: ['http://localhost:5173', 'https://table-booking-system-1.onrender.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
