1a1e048d11fb906c0f2b7329d3385546056a17e7
ahkhan13 <khanahs3351@gmail.com>
2025-01-05 19:55:59 +0530

    heading changes

--- front_end/src/components/CalendarView.jsx
+++ front_end/src/components/CalendarView.jsx
@@ -5,6 +5,7 @@ import 'react-calendar/dist/Calendar.css';
 const CalendarView = ({ onDateSelect }) => {
   return (
     <div className="p-4">
+       <h1 className='text-white text-center'>click on date to check availability</h1>
       <Calendar onChange={onDateSelect} />
     </div>
   );
